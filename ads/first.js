c1ddcea907a69be28096f616c204ae4ad5f60c7d
Cod Mark <95895380+CodMark@users.noreply.github.com>
2023-01-14 17:57:36 -0800

    js

---
+++ ads/first.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : 'bf7198d0a430a91ed06a946514867f47',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivecreativeformat.com/bf7198d0a430a91ed06a946514867f47/invoke.js"></scr' + 'ipt>');
